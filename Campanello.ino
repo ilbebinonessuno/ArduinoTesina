a100e78c9959ab9bc593fdd9d56e0ba35c670ffd
ilbebinonessuno <107624712+ilbebinonessuno@users.noreply.github.com>
2022-11-19 13:12:01 +0100

    Add comments

--- Campanello.ino
+++ Campanello.ino
@@ -1,16 +1,16 @@
-#define BUZZ 12
-#define BUTTON 11
-#define LEDSORDI 13
+#define BUZZ 12                             //Definisce il buzz sul pin 12
+#define BUTTON 11                           //Definisce il pulsante sul pin 11
+#define LEDSORDI 13                         //Definisce il led sul pin 13
 
 void setup(){
 
-  pinMode(BUZZ, OUTPUT);
-  pinMode(BUTTON, INPUT);
-  pinMode(LEDSORDI, OUTPUT);
+  pinMode(BUZZ, OUTPUT);                    //Definisce il buzz su come OUTPUT
+  pinMode(BUTTON, INPUT);                   //Definisce il pulsante su come INPUT
+  pinMode(LEDSORDI, OUTPUT);                //Definisce il LED su come OUTPUT
 }
 
 void loop() {
-  if (digitalRead(BUTTON) == HIGH) {
+  if (digitalRead(BUTTON) == HIGH) {        //Se il pulsante viene premuto il buzz riproduce il suono del campanello e fa lampeggiare il led 
     digitalWrite(LEDSORDI, HIGH);
     tone(BUZZ, 587, 200);
     delay(600);
